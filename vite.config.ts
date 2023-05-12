8832722ba65882ad3e36b20232369bdba10bcc53
Valianton <Valianton@mail.ru>
2023-05-12 14:20:58 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Valianton/Vue3.DraggableInput/'
+  base: '/Vue3.DraggableInput/'
 })
